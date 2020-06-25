9837d156e37f8b1564d64ef1d93c3c813e1c4f6f
Daniil.Suchkov <rewasors@gmail.com>
2020-06-26 01:31:29 +0300

    add next link

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,21 @@
 // build scene
-var scene = new ScrollMagic.Scene({
-    triggerElement: "#trigger1"
-})
-.setTween("#rocket", 0.6, {scale: 2.5}) // trigger a TweenMax.to tween
-.addIndicators({name: "1 (duration: 1)"}) // add indicators (requires plugin)
-.addTo(controller);
+// var scene = new ScrollMagic.Scene({
+//     triggerElement: "#trigger1"
+// })
+// .setTween("#rocket", 0.6, {scale: 2.5}) // trigger a TweenMax.to tween
+// .addIndicators({name: "1 (duration: 1)"}) // add indicators (requires plugin)
+// .addTo(controller);
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".rocket_IMG", {
+    scrollTrigger: {
+      trigger: ".header",
+      scrub: true,
+      pin: true,
+      start: " top",
+      end: "+=100%"
+    },
+    scaleX: 0, 
+    transformOrigin: "left center", 
+    ease: "none"
+  });
