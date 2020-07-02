61e6b6120e3dcab0783696f44df304845afcbc6c
Daniil.Suchkov <rewasors@gmail.com>
2020-07-03 01:31:55 +0300

    small fix animation

--- js/animation.js
+++ js/animation.js
@@ -69,7 +69,7 @@ var tl = gsap.timeline({
     pinSpacing: false,
     end: "+=100%",
     pinReparent: true,
-    onUpdate: self => console.log("progress:", self.progress)
+    // onUpdate: self => console.log("progress:", self.progress)
   }
 });
 
@@ -103,10 +103,13 @@ tl.addLabel("start")
   .to(".rocket_IMG_fire", {opacity: 1})
   .to(".rocket_IMG", {opacity: 0})
   .to(".rocket_IMG_fire", {y: -300, duration: 15000})
+  .to(".rocket_IMG_fire_stars", {y: -300, duration: 15000})
   .to(".earth_IMG", {opacity: 0, y: 400 , duration: 15000})
-  .to(".rocket_IMG_fire", {rotate: 45, duration: 5000})
-  .to(".rocket_IMG_fire", {rotate: 45, duration: 5000})
-  .to(".rocket_IMG_fire", {x: 600 , opacity: 0, duration: 25000})
+  .to(".rocket_IMG_fire_stars", {opacity: 1})
+  .to(".rocket_IMG_fire", {opacity: 0})
+  .to(".rocket_IMG_fire_stars", {rotate: 45, duration: 10000})
+  .to(".rocket_IMG_fire_stars", {rotate: 45, duration: 10000})
+  .to(".rocket_IMG_fire_stars", {x: 600 , opacity: 0, duration: 40000})
   .fromTo(".header__mainTitle", {x: -50, opacity: 0}, {x: 0, opacity: 1, duration: 40000})
   .fromTo(".header__secondTitle", {x: -50, opacity: 0}, {x: 0 , opacity: 1, duration: 40000})
   .fromTo(".header__title__horizontalLine", {x: -50, opacity: 0}, {x: 0 , opacity: 1, duration: 40000})
@@ -114,3 +117,27 @@ tl.addLabel("start")
   // .from(".header__mainTitle", {x: -600})
   // .from(".header__secondTitle", {x: -600})
   // .from(".header__title__horizontalLine", {x: -600})
+
+
+//   var latestKnownScrollY = $('html').scrollTop() || $('body').scrollTop();
+// $(window).on("scroll", function () {
+//     latestKnownScrollY = $('html').scrollTop() || $('body').scrollTop();
+// });
+// $(window).on("mousewheel DOMMouseScroll", function(event) {
+//     var scrollTo,
+//           scrollDistance  = 20,
+//           delta;
+//     if (event.type == 'mousewheel') {
+//           delta = event.originalEvent.wheelDelta / 120;
+//     } else if (event.type == 'DOMMouseScroll') {
+//           delta = - event.originalEvent.detail / 2;
+//     }
+//     scrollTo = latestKnownScrollY - delta * scrollDistance;
+//     if (scrollTo) {
+//         event.preventDefault(); 
+//         event.stopPropagation(); 
+//         $('body,html').stop().animate( { 
+//             scrollTop : scrollTo
+//          } , 700);
+//      }  
+// } );
